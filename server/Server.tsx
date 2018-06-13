1c74538684b7c067c8c767c8d619b45168ecab2b
Lyle Pratt <lylepratt@gmail.com>
2018-06-13 09:40:02 -0500

    initial commit

---
+++ server/Server.tsx
@@ -0,0 +1,33 @@
+import { createServer } from "http";
+import { Server as WebSocketServer } from "ws";
+
+import { connectedClients } from "./ConnectedClients";
+import { WebSocketTransport } from "metaverse-api";
+import RemoteScene from "./RemoteScene";
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws, req) {
+  const client = new RemoteScene(WebSocketTransport(ws));
+
+  client.on("error", (err: Error) => {
+    console.error(err);
+    ws.close();
+  });
+
+  connectedClients.add(client);
+
+  ws.on("close", () => connectedClients.delete(client));
+});
+
+server.listen(8087, function listening() {
+  console.log(`Listening on 8087`);
+});
---
+++ server/index.tsx
@@ -0,0 +1,18 @@
+/// <reference path="typings/generate-maze.d.ts" />
+
+// We use esnext in metaverse-api, webpack and rollup handle it natively but not Node.js
+const traceur = require('traceur')
+
+// replace node.js require by traceur's
+traceur.require.makeDefault(
+  function(filename: string) {
+    // transpile every file
+    return true
+  },
+  {
+    asyncFunctions: true,
+    asyncGenerators: true
+  }
+)
+
+require('./Server')
